ad20fa515602cacaa1129283c90d8f43c7fe1d6c
wade-sam <Sammy@idnet.co.uk>
2017-03-05 20:35:53 +0000

    Miracle Pill's App
    This is the first iOS app I’ve created

--- miraclePill/ViewController.swift
+++ miraclePill/ViewController.swift
@@ -8,10 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    
+    @IBOutlet weak var countyPicker: UIPickerView!
+    @IBOutlet weak var CountyPickerBtn: UIButton!
+    
+    
+    let Counties = ["Suffolk", "Cambridgeshire", "Liverpool", "Northampton", "Essex", "Cardiff", "Loughborough"]
     override func viewDidLoad() {
         super.viewDidLoad()
+        countyPicker.dataSource = self
+        countyPicker.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +28,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
-}
+    @IBAction func countyBtnPressed(_ sender: Any) {
+        countyPicker.isHidden = false
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return Counties.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return Counties[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        //countyBtnPressed.setTitle(Counties[row], for: UIControlState.application)
+        //countyPicker.isHidden = true
+        CountyPickerBtn.setTitle(Counties[row], for: UIControlState.normal)
+        countyPicker.isHidden = true
+    }
+} 
 
